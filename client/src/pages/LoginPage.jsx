e0ce0b0120542d5b47644393b7236ca61c375f64
Parth Gandhi <95434985+iamparth2002@users.noreply.github.com>
2024-02-13 16:28:41 +0530

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
 
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/api/login', {
+    const response = await fetch('https://mern-stack-app-beta.vercel.app/api/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-type': 'application/json' },
