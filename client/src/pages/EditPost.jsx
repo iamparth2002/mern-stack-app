d37324e11fb94e38e7cbfd4647b7ed2a63db9dda
Parth Gandhi <95434985+iamparth2002@users.noreply.github.com>
2024-02-13 16:27:33 +0530

    Update EditPost.jsx

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -12,7 +12,7 @@ const EditPost = () => {
     const [redirect,setRedirect] = useState(false);
   
     useEffect(() => {
-      fetch('http://localhost:4000/api/post/'+id)
+      fetch('https://mern-stack-app-beta.vercel.app/api/post/'+id)
         .then(response => {
           response.json().then(postInfo => {
             setTitle(postInfo.title);
@@ -32,7 +32,7 @@ const EditPost = () => {
       if (files?.[0]) {
         data.set('file', files?.[0]);
       }
-      const response = await fetch('http://localhost:4000/api/post', {
+      const response = await fetch('https://mern-stack-app-beta.vercel.app/api/post', {
         method: 'PUT',
         body: data,
         credentials: 'include',
